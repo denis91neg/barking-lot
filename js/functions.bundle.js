f97f33c78409a6b8e4532c884faee9dda579bd4e
denis91neg <y*********@gmail.>
2025-01-21 05:26:49 +0200

    1

--- js/functions.bundle.js
+++ js/functions.bundle.js
@@ -9,7 +9,7 @@ document.addEventListener('DOMContentLoaded', () => {
 	  primaryMenu.classList.toggle('is-expanded-menu');
 	});
   });
-(function (global, factory)) {
+(function (global, factory) {
 	typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
 	typeof define === 'function' && define.amd ? define(factory) :
 	( global = typeof globalThis !== 'undefined' ? globalThis : global || self, global.SEMICOLON = factory() );
